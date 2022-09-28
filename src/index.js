c5c4dd010be880aac192f52a89e33d94e5a6bfba
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-09-28 16:10:25 +0300

    Update styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './style.css';
 import populateTasks from './modules/populateTasks.js';
+import myTaskList from './modules/TaskList.js';
 
 window.onload = () => {
   populateTasks();
+
+  const addTaskBtn = document.querySelector('.add-task');
+
+  addTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newTask = document.querySelector('.new-task');
+    myTaskList.addTask(newTask.value, false, myTaskList.tasks.length);
+    console.log(myTaskList);
+    populateTasks();
+  });
 };

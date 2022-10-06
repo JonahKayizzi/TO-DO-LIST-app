ceec6a6d448b1285ef010bdb32ed8d025427967b
Shedrack Sunday <Hannania002@gmail.com>
2022-10-06 18:20:02 +0100

    Add jest test to confrim state

--- src/modules/index.test.js
+++ src/modules/index.test.js
@@ -10,10 +10,10 @@ describe('Updated task description', () => {
 
 describe('Updated task completion status', () => {
   test('Completed status should change from previous state', () => {
-    myTaskList.tasks.
-    const prevSta = myTaskList.tasks
+    const prevSta = myTaskList.tasks[1].completed;
     myTaskList.completionStatus(2);
-    expect()
+    const currSta = myTaskList.tasks[1].completed
+    expect(currSta).not.toEqual(prevSta);
   });
 });
 
